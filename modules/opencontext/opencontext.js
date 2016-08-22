616ae46ec20e36cbda070346fb5dcfc78949d591
Eric Kansa <kansaeric@gmail.com>
2016-08-22 13:59:32 -0700

    Start edits for Open Context new JSON-LD schema
    Not done yet, not tested.

--- modules/opencontext/opencontext.js
+++ modules/opencontext/opencontext.js
@@ -3,20 +3,46 @@
 define(['jquery'], function($) {
     return {
         name: 'Open Context Resource',
-        dataType: 'xml',
+        dataType: 'json',
         type: 'object',
         toDataUri: function(uri) {
             return uri;
         },
-        parseData: function(xml) {
+        parseData: function(data) {
             var getText = awld.accessor(xml);
-            var imageURI = getText('[id = "all_media"] img', 'src');
-            imageURI = typeof imageURI === 'string'? imageURI : imageURI[0];
-            return {
-                name: "OpenContext " + getText('[id = "item_name"]'),
-                description: getText('[id = "item_class"]'),
-                imageURI: imageURI,
-            };
+            data.name = data['dc-terms:title'];
+            data.description = ''; // tbd get item category
+            var imageURI = null;
+            if('oc-gen:has-obs' in data){
+                // get an imageURI from the item observations
+                imageURI = eachRecursiveKeyFind(data['oc-gen:has-obs'], 'oc-gen:thumbnail-uri');
+            }
+            if(imageURI == null && 'oc-gen:has-files' in data){
+                // get an image URI from the item files (if a media item)
+                // first one is a thumbnail
+                imageURI = eachRecursiveKeyFind(data['oc-gen:has-files'], 'id');
+            }
+            data.imageURI = imageURI;
+            return data;
         },
     };
 });
+
+function eachRecursiveKeyFind(obj, search_key){
+    // recursively goes through an object to find a key, returns its value if found
+    var output = null;
+    for (var key in obj){
+        if (key != search_key){
+            // the current key is not the search_key
+            if (output == null){
+                if (typeof obj[key] == "object" && obj[key] !== null){
+                    output = eachRecursive(obj[key]);
+                }
+            }
+        }
+        else{
+            output = obj[key];
+        }
+    } 
+    return output;
+}
